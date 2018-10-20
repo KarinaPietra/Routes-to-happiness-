14474d408ccaee3bb32ca1bc7a6f86e2414d8833
Karina Pietra <karinapietra95@gmail.com>
2018-10-20 13:26:47 -0400

    homework

---
+++ src/index.js
@@ -0,0 +1,44 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import './CSS/index.css';
+import {Link, Route, BrowserRouter as Router} from 'react-router-dom'
+import Happy from './components/Happy';
+import Sleepy from './components/Sleepy';
+import Guilty from './components/Guilty';
+
+// your code goes here
+class Home extends Component{
+  render(){
+    return(
+      <Router>
+        <div>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/happy">Happy</Link>
+            </li>
+            <li>
+              <Link to="/sleepy">Sleepy</Link>
+            </li>
+            <li>
+              <Link to="/guilty">Guilty</Link>
+            </li>
+          </ul>
+
+          <hr />
+
+            <Route exact path="/" />
+            <Route exact path="/happy" component={Happy}/>
+            <Route exact path="/sleepy" component={Sleepy}/>
+            <Route exact path="/guilty" component={Guilty}/>
+        </div>
+      </Router>
+    );
+  }
+}
+
+
+
+ReactDOM.render( <Home />, document.getElementById('root') );
